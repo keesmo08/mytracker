4d0dc35ea1a31d16dbf43d81d5b0bb806053c5da
Matthew Keesey <mattkeesey@hotmail.com>
2014-08-05 08:50:56 -0500

    initial commit

---
+++ app/views/calls/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @call, :id, :results, :spoken, :rvm, :lvm, :date, :time, :created_at, :updated_at
---
+++ db/migrate/20140803222407_create_calls.rb
@@ -0,0 +1,14 @@
+class CreateCalls < ActiveRecord::Migration
+  def change
+    create_table :calls do |t|
+      t.string :results
+      t.boolean :spoken
+      t.boolean :rvm
+      t.boolean :lvm
+      t.date :date
+      t.time :time
+
+      t.timestamps
+    end
+  end
+end
