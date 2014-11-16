2a2ab3b843d98bf7ae995ba7231dc0524c651e41
Matthew Keesey <mattkeesey@hotmail.com>
2014-11-16 10:14:15 -0600

    Add to 2 items to config/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,11 @@ Mytracker::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # NEEDED TO ADDED - from Bootstrap Can't get CSS working on Heroku using Rails 4
+  config.cache_classes = true
+  config.serve_static_assets = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
